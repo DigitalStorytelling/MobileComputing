ac4b9e64af63a71b7504c68dbe1ce112dcdaf4ba
lena105 <lena.scheit97@yahoo.de>
2024-04-05 17:03:38 +0200

    Navigator and first Station added

--- app/src/main/java/com/example/myearth/FirstStation.java
+++ app/src/main/java/com/example/myearth/FirstStation.java
@@ -1,8 +1,6 @@
 package com.example.myearth;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,18 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class FirstStation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Button loginButton = findViewById(R.id.button_login);
-        loginButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, Navigator.class);
-            startActivity(intent);
-        });
+        setContentView(R.layout.activity_first_station);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/myearth/Navigator.java
+++ app/src/main/java/com/example/myearth/Navigator.java
@@ -10,19 +10,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Navigator extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        Button loginButton = findViewById(R.id.button_login);
-        loginButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, Navigator.class);
+        setContentView(R.layout.activity_navigator);
+        Button firstStation = findViewById(R.id.button_first_station);
+        firstStation.setOnClickListener(v -> {
+            Intent intent = new Intent(Navigator.this, FirstStation.class);
             startActivity(intent);
         });
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.navigator), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
