e6fd399078c7387325a7d415853fc766aef955f6
Janina <feder.tinte@gmail.com>
2024-04-15 21:33:46 +0200

    Picaso hinzugefügt Aufgabe 2

--- app/src/main/java/com/example/myearth/MainActivity.java
+++ app/src/main/java/com/example/myearth/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.myearth;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,7 +20,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         Button loginButton = findViewById(R.id.button_login);
+
+        ImageView imageView = findViewById(R.id.imageview);
+
+        Picasso.get().load("https://cdn-icons-png.flaticon.com/512/4778/4778362.png")
+                .into(imageView);
+
         loginButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, Navigator.class);
             startActivity(intent);
