cb1b4cdac17ce9b387005fc9c907ab89a14729b2
Janina <feder.tinte@gmail.com>
2024-04-23 10:39:52 +0200

    Objekt Erkennung eingebaut in Schritt 3

--- app/src/main/java/com/example/myearth/ObjectDetection.java
+++ app/src/main/java/com/example/myearth/ObjectDetection.java
@@ -2,7 +2,8 @@ package com.example.myearth;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
@@ -11,24 +12,27 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ThirdStation extends AppCompatActivity {
+public class ObjectDetection extends AppCompatActivity {
+
+    WebView myWeb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_third_station);
+        setContentView(R.layout.activity_object_detection);
+
+        myWeb = findViewById(R.id.objectDetectionWeb);
+        myWeb.getSettings().setJavaScriptEnabled(true);
+        myWeb.setWebViewClient(new WebViewClient());
+        myWeb.loadUrl("https://rtod.vercel.app/");
+
         ImageButton back = findViewById(R.id.button_back);
         back.setOnClickListener(v -> {
-            Intent intent = new Intent(ThirdStation.this, Navigator.class);
+            Intent intent = new Intent(ObjectDetection.this, ThirdStation.class);
             startActivity(intent);
         });
 
-        ImageButton objectDetection = findViewById(R.id.objectDetection);
-        objectDetection.setOnClickListener(v -> {
-            Intent intent = new Intent(ThirdStation.this, ObjectDetection.class);
-            startActivity(intent);
-        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
